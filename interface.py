22623f33c7c459e9b29cf923b7d4c1cdfb748cfd
iguedes <igorguedrodrigues@gmail.com>
2019-04-19 00:44:20 -0300

    Test interface

--- interface.py
+++ interface.py
@@ -3,20 +3,8 @@ from tkinter import *
 
 class Application:
 	def __init__(self, master=None):
-		#Primeiro Container
-		self.primeiroContainer = Frame(master)
-		#self.primeiroContainer["pady"] = 10
-		self.primeiroContainer.pack()
-
-		#Segundo Container
-		self.segundoContainer = Frame(master)
-		#self.segundoContainer["padx"] = 20
-		self.segundoContainer.pack()
-
-		#terceiro container
-		self.terceiroContainer = Frame(master)
-		#self.terceiroContainer["pady"] = 20
-		self.terceiroContainer.pack()
+		#Lista de Containers
+		self.containers = []
 
 		#Titulo
 		self.titulo = Label(self.primeiroContainer, text="Torre de Hanoi")
