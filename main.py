d64b25acd7f0b23f0acaade7efc208af317abe29
Alexadre Batistella Bellas <alexandre.bellas@usp.br>
2019-04-18 23:23:49 -0300

    quit() question finalized

--- main.py
+++ main.py
@@ -10,15 +10,13 @@ from models import Pin, State
 import time
 import os
 
-passos = 0
-
 #Função de busca em profundidade (dfs)
 def dfs(estado, states_gone=[]):
 	if estado.pins[0].isEmpty() and (estado.pins[1].isEmpty()):
 		print("----------------Estado Atual----------------")
 		estado.printState()
 		print("Estado final alcançado!")
-		quit()
+		return 0
 
 	#Printa no terminal como esta o estado atual
 	print("----------------Estado Atual----------------")
@@ -55,15 +53,16 @@ def dfs(estado, states_gone=[]):
 		print("passos: %d " % passos)
 
 		#Recursivamente, busca em profundidade no filho
-		dfs(est, states_gone)
+		if(dfs(est, states_gone) == 0):
+			return 0
+		else:
+			#Se volta da recursão, é feito um passo a mais
+			passos += 1
+			print("passos: %d" % passos)
 
-		#Se volta da recursão, é feito um passo a mais
-		passos += 1
-		print("passos: %d" % passos)
-
-		#Na volta da recursao printa na tela o estado que esta voltando
-		print("----------------Estado Atual----------------")
-		estado.printState()
+			#Na volta da recursao printa na tela o estado que esta voltando
+			print("----------------Estado Atual----------------")
+			estado.printState()
 
 
 
@@ -76,6 +75,17 @@ First State for 3 pieces:
 pin1  pin2  pin3
 """
 #creating the first state for a Tower of Hanoi with 3 pieces:
+# s = State()
+# place_holder_father = State()
+# place_holder_father.addState(s)
+
+
+# N = 3
+# for i in range(N):
+# 	s.pins[0].push(N-i-1)
+
+# s.addFather(place_holder_father)
+
 s = State()
 place_holder_father = State()
 place_holder_father.addState(s)
@@ -87,6 +97,16 @@ for i in range(N):
 
 s.addFather(place_holder_father)
 
-dfs(s)
+while True:
+	input("Oi, eu sou o potato. Aperte enter para continuar com a DFS!")
+	
+	passos = 0
+	
+	dfs(s, [])
+	
+	print("Muito bem! Cabou um DFS. Perae um minuto.")
+	time.sleep(2)
+
+	os.system("clear")
 
 print("Numero de passos %d" % passos)
