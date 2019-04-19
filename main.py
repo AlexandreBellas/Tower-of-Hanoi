de4ab2c22f17395962445128f4d11743c077aef2
ThiagoMusico <thiago.musico50@gmail.com>
2019-04-19 00:28:05 -0300

    Add a class for depth search

--- main.py
+++ main.py
@@ -9,62 +9,7 @@ This is the main archive of the Tower of Hanoi problem
 from models import Pin, State
 import time
 import os
-
-#Função de busca em profundidade (dfs)
-def dfs(estado, states_gone=[]):
-	if estado.pins[0].isEmpty() and (estado.pins[1].isEmpty()):
-		print("----------------Estado Atual----------------")
-		estado.printState()
-		print("Estado final alcançado!")
-		return 0
-
-	#Printa no terminal como esta o estado atual
-	print("----------------Estado Atual----------------")
-	estado.printState()
-
-	#Gera automaticamente os proximos estados
-	estado.generateNextStates()
-
-	#Printa todos os filhos do estado atual
-	#estado.printNeighbors()
-
-	flag = 0
-
-	#Para cada estado filho
-	for est in estado.next_states:
-		
-		#verificar se já não se passou nele
-		for gone in states_gone:
-			if not est.isItemDifferent(gone):
-				#Se já passou, flag recebe 1
-				flag = 1
-				break
-		
-		#Essa verificação é necessária para ir ao próximo estado
-		if flag == 1:
-			break
-
-		#Se não passou, acrescenta-se aos estados já passados
-		states_gone.append(est)
-
-		#Acrescenta-se o número de passos
-		global passos
-		passos += 1
-		print("passos: %d " % passos)
-
-		#Recursivamente, busca em profundidade no filho
-		if(dfs(est, states_gone) == 0):
-			return 0
-		else:
-			#Se volta da recursão, é feito um passo a mais
-			passos += 1
-			print("passos: %d" % passos)
-
-			#Na volta da recursao printa na tela o estado que esta voltando
-			print("----------------Estado Atual----------------")
-			estado.printState()
-
-
+from depthfirst import Dfs
 
 """
 First State for 3 pieces:
@@ -97,12 +42,14 @@ for i in range(N):
 
 s.addFather(place_holder_father)
 
+
 while True:
 	input("Oi, eu sou o potato. Aperte enter para continuar com a DFS!")
 	
 	passos = 0
-	
-	dfs(s, [])
+
+	profundidade = Dfs(passos)
+	profundidade.busca(s, [])
 	
 	print("Muito bem! Cabou um DFS. Perae um minuto.")
 	time.sleep(2)
