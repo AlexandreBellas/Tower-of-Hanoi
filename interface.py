7ee76119450d9a87de1758a610bc0e096748caaa
iguedes <igorguedrodrigues@gmail.com>
2019-04-19 00:53:42 -0300

    Added buttons to interface

--- interface.py
+++ interface.py
@@ -4,22 +4,32 @@ from tkinter import *
 class Application:
 	def __init__(self, master=None):
 		#Lista de Containers
-		self.containers = []
+		self.containers = [Frame(master), Frame(master), Frame(master), Frame(master)]
+		for c in self.containers:
+			c.pack()
 
 		#Titulo
-		self.titulo = Label(self.primeiroContainer, text="Torre de Hanoi")
+		self.titulo = Label(self.containers[0], text="Torre de Hanoi")
 		self.titulo["font"] = ("Arial", "10", "bold")
 		self.titulo.pack()
 
 		#Botoes:
-		self.dfs = Button(self.terceiroContainer)
+		self.dfs = Button(self.containers[2])
 		self.dfs["text"] = "Busca em Profundidade"
 		self.dfs["font"] = ("Arial", "10")
 		self.dfs.bind("<Button-1>", self.buscaProfundidade)
 		self.dfs.pack(side=LEFT)
+		self.hc = Button(self.containers[2])
+		self.hc["text"] = "Busca Hill Climbing"
+		self.hc["font"] = ("Arial", "10")
+		self.hc.bind("<Button-1>", self.buscaHillClimbing)
+		self.hc.pack(side=RIGHT)
 
 	def buscaProfundidade(self, event):
-		print("teste")
+		print("teste1")
+
+	def buscaHillClimbing(self, event):
+		print("teste2")
 
 
 root = Tk()
