81ef36a9d9fa7d64b809a498bea76cb4a1642407
iguedes <igorguedrodrigues@gmail.com>
2019-04-21 15:32:24 -0300

    Draw State may be done, needs testing

--- interface.py
+++ interface.py
@@ -63,13 +63,38 @@ class Interface(Frame):
 		ButtonDFS.pack(side=LEFT, padx=50, pady=40)
 		ButtonHC.pack(side=RIGHT, padx=50, pady=20)
 
-	def drawState(self):
+	def drawState(self, state):
+		#Apaga o Canvas
 		self.Canvas.delete("all")
+
+		#Define o tamanho dos pinos
+		pinWidth = 10
+		pinHeight = 300
+
+		#Define o tamanho do menor bloco
+		minBlockWidth = self.canvasWidth/6*self.pieces
+		minBlockHeight = pinHeight/self.pieces
+
 		#s = State()
 		#state.copyState(s)
-		self.Canvas.create_rectangle(self.canvasWidth/4 -5, self.canvasHeight - 300, self.canvasWidth/4 + 5, self.canvasHeight, fill="black")
-		self.Canvas.create_rectangle(self.canvasWidth/2 - 5, self.canvasHeight - 300, self.canvasWidth/2 + 5, self.canvasHeight, fill="black")
-		self.Canvas.create_rectangle(3*self.canvasWidth/4 -5, self.canvasHeight - 300, 3*self.canvasWidth/4 + 5, self.canvasHeight, fill="black")
+
+		#Criando os Pinos
+		self.Canvas.create_rectangle(self.canvasWidth/4 -pinWidth/2, self.canvasHeight -pinHeight, self.canvasWidth/4 +pinWidth/2, self.canvasHeight, fill="black")
+		self.Canvas.create_rectangle(self.canvasWidth/2 -pinWidth/2, self.canvasHeight -pinHeight, self.canvasWidth/2 +pinWidth/2, self.canvasHeight, fill="black")
+		self.Canvas.create_rectangle(3*self.canvasWidth/4 -pinWidth/2, self.canvasHeight -pinHeight, 3*self.canvasWidth/4 +pinWidth/2, self.canvasHeight, fill="black")
+
+		for j in range(len(state.pins)):
+			towerHeight = 0
+			for i in range(0, state.pins[j].size()):
+				block = state.pins[j].pop()
+				blockX0 = ((j+1)self.canvasWidth/4) - minBlockWidth*(block+1)/2
+				blockY0 = self.canvasHeight - (towerHeight + minBlockHeight*(block+1))
+				blockX1 = ((j+1)*self.canvasWidth/4) + minBlockWidth*(block+1)/2 + 1
+				blockY1 = self.canvasHeight - towerHeight
+				towerHeight += minBlockHeight*(block+1) + 1
+				self.Canvas.create_rectangle(blockX0, blockY0, blockX1, blockY1, fill="grey")
+
+
 
 root = Tk()
 interface = Interface(master=root)
