dfcbbad9775f5004e08f44eaa8e8bc63da608638
Alexadre Batistella Bellas <alexandre.bellas@usp.br>
2019-04-22 02:53:27 -0300

    Finalizing comments and minimal changes

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ import os
 #Executará até o fim
 while True:
 	#É necessária uma entrada numérica
-	num_pieces = int(input("Número de peças (insira 0 para sair): "))
+	try:
+		num_pieces = int(input("Número de peças (insira 0 para sair): "))
+	except ValueError:
+		os.system("clear")
+		print("Insira números válidos.")
+		continue
 
 	#Condição de saída do programa
 	if num_pieces == 0:
@@ -29,9 +34,9 @@ while True:
 
 	#Realização da busca DFS
 	busca.dfs(s, [])
-	input("Final do algoritmo DFS. Pressione enter para seguir para a heurística Hill Climbing.")
+	input("Final do algoritmo DFS. Pressione enter para seguir para a heurística Hill Climbing.\n")
 
 	busca.hillClimbing(s)
-	input("Final da heurística Hill Climbing. Pressione enter para reiniciar.")
+	input("Final da heurística Hill Climbing. Pressione enter para reiniciar.\n")
 
 	os.system("clear")
